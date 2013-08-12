a5bec681e1c771b4c4db57898f50a88a5794bc77
Djalma araujo <djalma.araujo@gmail.com>
2013-08-11 23:08:11 -0300

    config example

--- app/scripts/config_example.js
+++ app/scripts/config_example.js
@@ -1,7 +1,15 @@
 (function(global) {
   var CONFIG = {
-    //
+    address: '//pejs:4000',
+    port: 4000,
+    timers: {
+      startup: 5000
+    },
+
+    messages: {
+      welcome_message: 'Waiting to start..'
+    }
   };
 
-  global.GAME_CONFIG = CONFIG;
+  global.SNOOKER_CONFIG = CONFIG;
 })(typeof global === "undefined" ? window : exports);
