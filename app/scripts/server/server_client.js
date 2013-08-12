b777bbef10dfadaaad12bc918b23667f0642a402
Djalma araujo <djalma.araujo@gmail.com>
2013-08-11 23:05:44 -0300

    add wait to start feature

--- app/scripts/server/server_client.js
+++ app/scripts/server/server_client.js
@@ -1,17 +1,43 @@
-(function (window, socket) {
-  var SnookerLiveServerClient = function (socket) {
-    this.socket = socket;
+(function (window, $) {
+  var SnookerLiveServerClient = function () {
+    this.bootstrap();
+  };
 
-    this.connect();
+  SnookerLiveServerClient.prototype.bootstrap = function () {
+    this.setDOMItems();
+    this.checkForStart();
+  };
+
+  SnookerLiveServerClient.prototype.checkForStart = function () {
+    if (SNOOKER_CONFIG.autoStart) {
+      this.socket = io.connect(SNOOKER_CONFIG.address);
+      this.connect();
+    }
+    else {
+      this.statusMessage.html(SNOOKER_CONFIG.messages.welcome_message);
+
+      setTimeout(function () {
+        window.location.href = '';
+      }, SNOOKER_CONFIG.timers.startup);
+    }
+  };
+
+  SnookerLiveServerClient.prototype.setDOMItems = function () {
+    this.container = $("#container");
+    this.ranking = $("#ranking");
+    this.snooker = $("#snooker");
+    this.statusMessage = $(".status-message");
   };
 
   SnookerLiveServerClient.prototype.connect = function () {
     var instance = this;
 
     socket.emit('whoami', {type: 'server'});
+
     socket.on('welcome', function (data) {
       if (data.status) {
         instance.handlers();
+        instance.prepareStage();
       }
     });
   };
@@ -20,5 +46,12 @@
     var instance = this;
   };
 
-  new SnookerLiveServerClient(socket);
-})(window, socket);
+  SnookerLiveServerClient.prototype.prepareStage = function () {
+    var instance = this;
+
+    instance.container.removeClass('disabled');
+    instance.statusMessage.hide();
+  };
+
+  new SnookerLiveServerClient();
+})(window, Zepto);
